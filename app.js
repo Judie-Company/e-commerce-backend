9f9feed5c46752e93594d0483e5fbd23eda8b14b
Judielyn Pagente <git-commit-address>
2023-08-12 12:56:41 +0800

    STEP: 3-9 Installing Mongoose and Connect to MongoDB Database

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // console.log("hello world");
 const express = require("express");
 const app = express();
-const morgan = require ('morgan');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 require("dotenv/config");
 
 const api = process.env.API_URL;
 
 //Middleware --> previous body parser
 app.use(express.json());
-app.use(morgan ('tiny'));
+app.use(morgan("tiny"));
 // http://localhost:3000/api/v1/products
 //Create a sample get request to route /
 
@@ -23,7 +25,6 @@ app. listen(3000, ()=>{
     console. log ('server is running http://localhost:3000');
 })
 */
-
 
 // 3-6 code edited code pushed
 app.get(`${api}/products`, (req, res) => {
@@ -43,6 +44,17 @@ app.post(`${api}/products`, (req, res) => {
     res.send(newProduct);
 });
 
+//Database Connection
+mongoose
+    .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log("Database Connection is ready...");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 app.listen(3000, () => {
     console.log(api);
     console.log("server is running http://localhost:3000");
